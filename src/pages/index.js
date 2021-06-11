a54876cbd25863af5db0b05daaa7f9e051fe7910
SimonSoftwareSolutions <simon.poppinga@gmail.com>
2021-06-11 23:56:42 +0200

    A

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
 import * as React from "react"
+import Grid from '@material-ui/core/Grid';
+import Modal from '@material-ui/core/Modal';
+
+import thumpnail from './../images/thumbnail.jpg'
+import kopf from './../images/kopf.jpg'
+
 
 // styles
 const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  backgroundColor : '9bf6ff'
 }
+
+const img = {
+  maxWidth: "60%",
+  height: "auto",
+ 
+}
+
+const imgBorderRadius = {
+  ...img,
+  borderRadius: '50%'
+}
+
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
@@ -78,107 +97,105 @@ const badgeStyle = {
   lineHeight: 1,
 }
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+const backgroundColor = {
+  backgroundColor : '#90e0ef'
+}
+
+
+const centeredItems = {
+  textAlign: 'center',
+}
+
+const headline = {
+  textAlign: 'center',
+  fontWeight: 300,
+  fontSize: 24,
+}
 
 // markup
 const IndexPage = () => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+    setOpen(true);
+    };
+
+    const handleClose = () => {
+    setOpen(false);
+    };
+
   return (
-    <main style={pageStyles}>
-      <title>Homee Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+   
+     
+      <Grid container spacing={3} style={backgroundColor}>
+      <title>Pandora Festival</title>
+
+      <Grid item xs={12} style={centeredItems}>
+        <img src={kopf} alt="Logo" style={img} />
+      </Grid>
+
+      {/* <Grid item xs={3} style={centeredItems}/>
+      <Grid item xs={6} style={centeredItems}>
+          <img src={thumpnail} alt="Logo" style={imgBorderRadius} />
+        </Grid>
+      <Grid item xs={3}/>
+       
+        */}
+
+      <Grid item xs={6} style={centeredItems}>
+        <h1>Packliste</h1>
+        <img src={thumpnail} alt="Logo" style={imgBorderRadius} />
+      </Grid>
+      <Grid item xs={6} style={centeredItems}>
+        <h1>Anfahrt</h1>
+        <img src={thumpnail} alt="Logo" style={imgBorderRadius} onClick={() => handleOpen()}/>
+      </Grid>
+
+      <Grid item xs={6} style={centeredItems}>
+        <h1>FAQ</h1>
+        <img src={thumpnail} alt="Logo" style={imgBorderRadius} />
+        </Grid>
+      <Grid item xs={6} style={centeredItems}>
+        <h1>Pioniere</h1>
+        <img src={thumpnail} alt="Logo" style={imgBorderRadius} />
+      </Grid>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        centered
+      >
+        {body}
+      </Modal>  
+
+
+      </Grid>
+
   )
 }
+
+const modalStyle = {
+  position: 'absolute',
+  width: 400,
+  backgroundColor: '#023e8a',
+  opacity: 0.8,
+  top: '40%',
+  left: '50%',
+  marginTop: '-50px',
+  marginLeft: '-50px'
+}
+
+const body = (
+  <Grid container spacing={3} style={modalStyle}>
+    <Grid item xs={12}>
+      <h1>Hey Festivalbesucher</h1>
+      <p>
+        Schön bla bla bla bla
+      </p>
+    </Grid>
+
+  </Grid>
+);
 
 export default IndexPage
